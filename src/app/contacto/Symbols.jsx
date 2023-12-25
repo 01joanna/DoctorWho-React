81d870d89f0d7734f3430552e7f57c5782401965
01joanna <joa.cuevas9@gmail.com>
2023-12-25 17:43:18 +0100

    added contact, conoce-a and doctors modifications, forgot to change branches, resolve later on new branches

--- src/app/contacto/Symbols.jsx
+++ src/app/contacto/Symbols.jsx
@@ -1,4 +1,6 @@
 import './contact.css'
+import SymbolPink from '../../../public/assets/img/symbols/persistencia.png'
+import SymbolOrange from '../../../public/assets/img/symbols/resistencia.png'
 
 export default function SymbolsContact() {
     return (
