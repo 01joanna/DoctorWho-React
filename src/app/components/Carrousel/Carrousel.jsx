f83fc3202d9e57bd92016534b31210c9752abd2b
01joanna <joa.cuevas9@gmail.com>
2023-12-27 12:56:44 +0100

    fixed css in carrousel

--- src/app/components/Carrousel/Carrousel.jsx
+++ src/app/components/Carrousel/Carrousel.jsx
@@ -1,9 +1,13 @@
 'use client'
 import { useState } from 'react'
 import './carrousel.css'
+import ButtonComponent from '../GeneralButton/GeneralButton'
 
-const image1 = '/assets/img/carrusel/carrusel-1.webp';
-const image2 = '/assets/img/carrusel/carrusel-2.jpg';
+const image1 = '/assets/img/carrusel/carrusel-2.jpg';
+const image2 = '/assets/img/carrusel/carrusel-1.webp';
+const image3 = '/assets/img/carrusel/carrusel-3.jpeg';
+const image4 = '/assets/img/carrusel/carrusel-4.jpeg';
+const image5 = '/assets/img/carrusel/carrusel-5.jpeg';
 
 const imagesCarousel = [{
     id: 0,
@@ -14,6 +18,19 @@ const imagesCarousel = [{
     title: 'Allons-y!',
     imgUrl: image2,
 
+},{
+    id: 2,
+    title: 'The Giggle',
+    imgUrl: image3,
+},{
+    id: 3,
+    title: 'Allons-y!',
+    imgUrl: image4,
+
+},{
+    id: 4,
+    title: 'The Giggle',
+    imgUrl: image5,
 }]
 
 const Carrousel = () => {
@@ -31,12 +48,18 @@ const Carrousel = () => {
 
     return (
         <div className='carousel-container'>
-            <div className='carousel-image' style={{ backgroundImage: `url(${imagesCarousel[currentImageIndex].imgUrl})` }}>
+            <div 
+            className='carousel-image' 
+            style={{ backgroundImage: `url(${imagesCarousel[currentImageIndex].imgUrl})` }}>
                 <h2>{imagesCarousel[currentImageIndex].title}</h2>
+                <ButtonComponent text='Ver más' to='https://www.doctorwho.tv/' className="carrousel__main-btn"/>
             </div>
             <div className='carousel-buttons'>
-                <button onClick={prevImage}>Hola1</button>
-                <button onClick={nextImage}>Hola2</button>
+                <button onClick={prevImage}></button>
+                <button onClick={nextImage}></button>
+                <button onClick={prevImage}></button>
+                <button onClick={nextImage}></button>
+                <button onClick={prevImage}></button>
             </div>
         </div>
     );
--- src/app/components/Contact/Contact.jsx
+++ src/app/components/Contact/Contact.jsx
@@ -34,7 +34,6 @@ export default function Contact() {
                     />
                 </div>
             </form>
-            <GeneralButton />
         </section>
     )
 };
