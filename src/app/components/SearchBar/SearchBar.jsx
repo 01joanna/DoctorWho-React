c8b3653648bbcf06540926f5e52428eaa9dd5181
01joanna <joa.cuevas9@gmail.com>
2024-01-04 15:17:07 +0100

    modified and commented everything to mske the search component work, finish it later

--- src/app/components/SearchBar/SearchBar.jsx
+++ src/app/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,15 @@
-// 'use client'
-// import { useState } from 'react';
+'use client'
+import { createContext, useState, useContext } from 'react';
 import SearchIcon from '../../../../public/assets/img/icons/material-symbols-light_search.svg'
-// import { useState } from "react";
 import Image from "next/image";
 import './searchbar.css'
 
+// const doctorsContext = createContext();
+// export function useDoctorsContext() {
+//     return useContext(doctorsContext);
+// }
 export default function SearchBar() {
-    // const [searchItem, setSearchItem] = useState("");  
-    // const handleInputChange = (e) => {
-    //     const searchTerm = e.target.value;
-    //     setSearchItem(searchTerm);
-    // }
+    // const [item, setItem] = useState("Buscar")
 
     return (
         <div className="searchbar__container">
@@ -24,13 +23,12 @@ export default function SearchBar() {
 
             <input 
             type="text"
-            placeholder="Buscar"
+            name={'search'}
+            // value={item}
+            placeholder={'Buscar'}
             className='searchbar__placeholder'
-            // value={searchItem}
-            // onChange={handleInputChange}
+            // onChange={(e) => handleChange(e.target.value)}
             />
         </div>
-
-        
     )
 }
